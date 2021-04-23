9c2b8f97de762cb2b1cf89c065be094727e865f3
SujalGupta244 <68183250+SujalGupta244@users.noreply.github.com>
2021-04-23 10:50:31 +0530

    Simple landing page
    simple landing page built with HTML and CSS

---
+++ main.js
@@ -0,0 +1,21 @@
+const navToggle = document.querySelector('.nav_toggle')
+const navList = document.querySelector('.nav_list')
+
+
+navToggle.addEventListener('click',()=>{
+    navList.classList.toggle('show_nav')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
